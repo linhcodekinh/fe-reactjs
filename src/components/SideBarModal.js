3d6348b1f438f8212b5524db289e96309159e3a5
linhcodekinh <nvlinh296@gmail.com>
2024-01-12 11:35:39 +0700

    update120124

--- src/components/SideBarModal.js
+++ src/components/SideBarModal.js
@@ -16,57 +16,149 @@ class SideBarModal extends Component {
     }
 
     componentDidMount() {
-        document.addEventListener('keydown', this.handlerKeyDown);
+
     }
 
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.handlerKeyDown);
-    }
+    // componentWillUnmount() {
+    //     document.removeEventListener('keydown', this.handlerKeyDown);
+    // }
+
+    // handlerKeyDown = (event) => {
+    //     const keyCode = event.which || event.keyCode;
+    //     if (keyCode === KeyCodeUtils.ENTER) {
+    //         if (!this.acceptBtnRef.current || this.acceptBtnRef.current.disabled) return;
+    //         this.acceptBtnRef.current.click();
+    //     }
+    // }
 
     toggle = () => {
         this.props.toggleSideBarModal();
     }
 
-
-
     render() {
         return (
-            <div
-                className={this.props.isOpen ? "modal-dialog show" : "modal-dialog"}
-                role="document"
-            >
-                <div className="modal-content">
-                    <div className="modal-header" style={{ height: "10%"}}>
-                        <button
-                            type="button"
-                            className="close"
-                            // data-dismiss="modal"
-                            aria-label="Close"
-                            onClick={() => {
-                                this.toggle()
-                            }}
-                        >
-                            <span aria-hidden="true">hehehe</span>
-                        </button>
-                        <h4 className="modal-title" id="myModalLabel">
-                            Rigth Sidebar
-                        </h4>
+                <div
+                    className={this.props.isOpen ? "modal-dialog show" : "modal-dialog"}
+                    role="document"
+                >
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button
+                                type="button"
+                                className="close"
+                                // data-dismiss="modal"
+                                aria-label="Close"
+                                onClick={() => {
+                                    this.toggle()
+                                }}
+                            >
+                                <span aria-hidden="true">hehehe</span>
+                            </button>
+                            <h4 className="modal-title" id="myModalLabel">
+                                Rigth Sidebar
+                            </h4>
+                        </div>
+
+                        <div className="modal-body">
+                            <p> fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfd
+                                fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
+                                fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
+                                fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
+                                sfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+
+
+                                v
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa
+
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+                                aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+
+                            </p>
+
+
+                        </div>
+
+                        <div className="modal-footer" style={{bottom: 0,marginBottom: '20px', position: "fixed", width: "10%", height: "10%", border: "1px solid gray", backgroundColor: "orange"}}>
+                            <p> footer</p>
+
+                        </div>
                     </div>
 
-                    <div className="modal-body" style={{ height: "80%"}}>
-                        <p> fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfd
-                        fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
-                        fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
-                        fgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds
-                        sfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfdsfgsdfds</p>
-                    </div>
-
-                    <div className="modal-footer" style={{ height: "10%"}}> 
-                    <p> footer</p>
-
-                    </div>
                 </div>
-            </div>
+
+
         );
     }
 
